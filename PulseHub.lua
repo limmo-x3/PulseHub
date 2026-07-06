a0224d1394ec54f161f0539276e9d4d337f7fa25
limmo-x3 <nigavaev3@gmail.com>
2026-07-06 15:18:39 +0400

    PulseHub.lua

--- PulseHub.lua
+++ PulseHub.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/limmo-x3/PulseHub/refs/heads/main/loadbar.txt"))()
 loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6ImMwNmY5ZDhlLTY3NzctNDcyYy04ZjMwLWU3MThlZGIwM2MyOSIsImtpbmQiOiJsb2FkZXIifQ"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/limmo-x3/PulseHub/refs/heads/main/loadbar.txt"))()
